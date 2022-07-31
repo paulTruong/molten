1ebbd39854cf6b5fcc244d84a19d80182f81dd1d
Paul Truong <mrpaultruong@gmail.com>
2022-07-31 17:34:09 +1000

    Moved fonts to theme.json
    Moved font loading from functions.php to theme.json. Also converted fonts from ttf to woff2 for better loading performance

--- functions.php
+++ functions.php
@@ -13,67 +13,15 @@ function molten_styles()
         $version_string
     );
 
-    // Add styles inline.
-    wp_add_inline_style('molten-style', molten_get_font_face_styles());
-
     // Enqueue theme stylesheet.
     wp_enqueue_style('molten-style');
 }
 add_action('wp_enqueue_scripts', 'molten_styles');
 
-function molten_editor_styles()
-{
-    // Add styles inline.
-    wp_add_inline_style('wp-block-library', molten_get_font_face_styles());
-}
-add_action('admin_init', 'molten_editor_styles');
-
-function molten_get_font_face_styles()
-{
-    return "
-    @font-face{
-        font-family: 'Playfair Display';
-        font-weight: 200 900;
-        font-style: normal;
-        font-stretch: normal;
-        font-display: swap;
-        src: url('" . get_theme_file_uri('assets/fonts/PlayfairDisplay-VariableFont_wght.ttf') . "') format('truetype');
-    }
-
-    @font-face{
-        font-family: 'Playfair Display';
-        font-weight: 200 900;
-        font-style: italic;
-        font-stretch: normal;
-        font-display: swap;
-        src: url('" . get_theme_file_uri('assets/fonts/PlayfairDisplay-Italic-VariableFont_wght.ttf') . "') format('truetype');
-    }
-	@font-face{
-		font-family: 'Source Sans Pro';
-		font-style: normal;
-		font-display: swap;
-		src: url('" . get_theme_file_uri('assets/fonts/SourceSansPro-Regular.ttf') . "') format('truetype');
-	}
-    @font-face{
-		font-family: 'Source Sans Pro';
-		font-style: italic;
-		font-display: swap;
-		src: url('" . get_theme_file_uri('assets/fonts/SourceSansPro-Italic.ttf') . "') format('truetype');
-	}
-    @font-face{
-		font-family: 'Source Sans Pro';
-		font-style: normal;
-        font-weight: 700;
-		font-display: swap;
-		src: url('" . get_theme_file_uri('assets/fonts/SourceSansPro-Bold.ttf') . "') format('truetype');
-	}
-	";
-}
-
 function molten_preload_webfonts()
 {
 ?>
-    <link rel="preload" href="<?php echo esc_url(get_theme_file_uri('assets/fonts/SourceSansPro-Regular.ttf')); ?>" as="font" crossorigin>
+    <link rel="preload" href="<?php echo esc_url(get_theme_file_uri('assets/fonts/source-sans-pro-regular.woff2')); ?>" as="font" crossorigin>
 <?php
 }
 add_action('wp_head', 'molten_preload_webfonts');
